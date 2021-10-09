1d7187ae1b0a64c53a43ff4782db95f945066e03
John Atanbori <j.atanbori@hull.ac.uk>
2021-10-09 08:19:08 +0100

    updated

---
+++ L3.7-TheLoyaltyScheme/TheLoyaltyScheme.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace L3._7_TheLoyaltyScheme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Loyalty Scheme.");
+
+            // Add all your codes here...
+            
+
+        }
+    }
+}
