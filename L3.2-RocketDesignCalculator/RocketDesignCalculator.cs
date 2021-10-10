5d0d0394cd8a3f92005850b0fa66e72a54fde958
John Atanbori <j.atanbori@hull.ac.uk>
2021-10-10 10:31:18 +0100

    updated

---
+++ L3.2-RocketDesignCalculator/RocketDesignCalculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace L3._2_RocketDesignCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Rocket Design Calculator");
+
+            //All your codes should go here
+
+            
+
+        }
+    }
+}
